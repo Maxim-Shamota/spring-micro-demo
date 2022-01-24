62ff342ad18668a75b9d50db26645307368cc503
Maxim <xbultazar@gmail.com>
2022-01-24 20:10:07 +0300

    доработанный проект для клиента eureka с добавленным restful сервисом

---
+++ eureka-client/src/test/java/site/shamota/micro/demo/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package site.shamota.micro.demo.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
